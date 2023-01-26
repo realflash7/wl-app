89eb2b2c4b5925c035ed3c90e2f877d20ca9001b
Piyush Singh <realflash@Piyushs-Mac-mini.local>
2023-01-26 18:27:48 +0530

    empty dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div style={{ margin: "1rem"}}>
+      <h1>Wishlink Dashboard</h1>
+      <Outlet></Outlet>
+
+
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         Wishlink Dashboard
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}>
+          <Route path="" element={<Dashboard/>}></Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Something awesome coming soon!</p>
+                <Link to="/">Home</Link>
+              </main>
+            }
+            />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
