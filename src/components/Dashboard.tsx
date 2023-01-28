ac073940ccdf886f6d68fdf6c0aa9ad09a52850b
Piyush Singh <realflash@Piyushs-Mac-mini.local>
2023-01-28 22:32:57 +0530

    simple in-app browser

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,36 @@
 import { Link } from "react-router-dom";
+import { Browser } from '@capacitor/browser';
+
+
 
 function Dashboard() {
+    const selectedTeam ='453';
+
+    const openCapacitorSite = async () => {
+        console.log('Opening in-app browser');
+        await Browser.open({ url: 'https://shopforaurelia.com/' });
+      };
+
     return(
         <>
         <Link to="/">Home</Link>
-        <h2>Dashboard</h2>
+        <h2>Dashboard 2</h2>
+        <form onSubmit={openCapacitorSite}>
+        <p>Choose from our Partner Brands</p>
+        <select name="targets" id="selectedTeam">
+            <optgroup label="e-commerce">
+            <option value="amazon">Amazon</option>
+            <option value="myntra">Myntra</option>
+            </optgroup>
+            <optgroup label="brands">
+            <option value="h&m">H&M</option>
+            <option value="aurelia">Aurelia</option>
+            </optgroup>
+        </select>
+            <button type="submit">open browser</button>
+        </form>
+        
+	
         </>
     )
 };
